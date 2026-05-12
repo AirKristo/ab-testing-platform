936769a04d929aca4642ef02ea304b70e4bdf954
Kristo <kp1404@nyu.edu>
2026-05-12 11:22:07 -0400

    Wire up App with routing, CartProvider, and Tailwind config

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Orders from './pages/Orders';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <CartProvider>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/orders" element={<Orders />} />
+          </Routes>
+        </div>
+      </CartProvider>
+    </BrowserRouter>
+  );
+}
